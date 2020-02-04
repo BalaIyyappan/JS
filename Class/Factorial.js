c1963219d262f4908a71fb7dba4b477bc846691e
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-04 22:04:54 +0530

    Update Factorial.js

--- Class/Factorial.js
+++ Class/Factorial.js
@@ -6,3 +6,16 @@ function fact(n){
   console.log(fact);
 }
 fact(5);
+
+// Using User Input
+/*
+function fact(num){
+    fact=1;
+    for(i=1;i<num;i++){
+        fact=fact+fact*i;
+    }
+    alert(fact);
+}
+var num=parseInt(prompt("Enter any number"));
+fact(num);
+*/
