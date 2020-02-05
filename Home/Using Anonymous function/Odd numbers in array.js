6f0a4b53b917a4168854e1f7dd42b06cf4b938a0
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-05 20:55:39 +0530

    Create Odd numbers in array.js

---
+++ Home/Using Anonymous function/Odd numbers in array.js
@@ -0,0 +1,11 @@
+var odd=function(){
+    var array=[];
+    var n=parseInt(prompt("Enter the range"));
+    for(var i=1;i<=n;i++){
+        if(i%2!=0){
+            array.push(i);
+        }
+    }
+    console.log(array);
+}
+odd();
