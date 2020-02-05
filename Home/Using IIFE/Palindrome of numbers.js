257d7d176cca1b3368b696ebc6d393fe9eb0e6bc
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-05 23:01:13 +0530

    Create Palindrome of numbers.js

---
+++ Home/Using IIFE/Palindrome of numbers.js
@@ -0,0 +1,18 @@
+(function(){
+    var arr=[11,12,33,56,77,121,100,111];
+    var pali=[];
+    for(var i=0;i<arr.length;i++){
+        var temp=arr[i];
+        var r=0; var p=0; var val=arr[i];
+        while(val>0){
+            r=val%10;
+            val=parseInt(val/10);
+            p=p*10+r;
+        }
+        if(temp==p){
+            pali.push(temp);
+        }
+    }
+    console.log(pali);
+})
+();
