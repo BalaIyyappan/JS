5368caec01f81aedbed21315aca3f41c73282be0
Bala Iyyappan V <43007978+BalaIyyappan@users.noreply.github.com>
2020-02-05 20:43:23 +0530

    Create To insert any word in between any two words.js

---
+++ Class/To insert any word in between any two words.js
@@ -0,0 +1,24 @@
+var car=['BMW','AUDI','BENZ','RR','JAGUAR'];
+/*
+car[car.length]=prompt("Enter any car");
+console.log(car);
+
+//To add any value in an array at any posotion
+car.splice(1,0,"FORD");
+console.log(car);
+*/
+//without using splice
+/* To Know the index of any element
+var p=car.indexOf("AUDI");
+console.log(p);
+*/
+for(var i=car.length-1;i>=0;i--){
+    car[i+1]=car[i];
+    
+    if(car[i]=="AUDI"){
+        car[i]="LEXUS";
+        break;
+    }
+
+}
+console.log(car);
